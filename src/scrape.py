c793132d0e87ae19683edfb899d34f3bcfed8094
Seungyeon Han <hanseungyeon11@gmail.com>
2024-11-06 20:56:04 +0100

    add extra features to extract

--- src/scrape.py
+++ src/scrape.py
@@ -8,16 +8,18 @@ def get_recipe(url):
     try:
         scraper = scrap_me(url)
         return {
+            'url': url,
             'title': scraper.title(),
+            'description' : scraper.description() if hasattr(scraper, 'description') else None,
             'ingredients': scraper.ingredients(),
             'instructions': scraper.instructions(),
             'picture_link': scraper.picture() if hasattr(scraper, 'picture') else None,
             'cooking_time': scraper.total_time(),
-            'servings': scraper.servings(),
-            'ratings': scraper.ratings(),
-            'tags': scraper.tags(),
-            'author': scraper.author(),
-            'publish_date': scraper.publish_date()
+            'servings': scraper.servings() if hasattr(scraper, 'servings') else None,
+            'ratings': scraper.ratings() if hasattr(scraper, 'ratings') else None,
+            'tags': scraper.tags() if hasattr(scraper, 'tags') else None,
+            'author': scraper.author() if hasattr(scraper, 'author') else None,
+            'publish_date': scraper.publish_date() if hasattr(scraper, 'publish_date') else None
 
         }
     except Exception as e:
