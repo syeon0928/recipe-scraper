57b3a86cce05495dfedca4e0e21f28e486590d75
Seungyeon Han <hanseungyeon11@gmail.com>
2024-11-06 20:56:34 +0100

    Add main function
    to gather recipes for each url and save as json file

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # main.py
 from src.config import *
 from src.gather_urls import get_recipe_links_from_menu, get_recipe_links_from_list, get_all_recipe_links
-from db.recipe_url_storage import load_links, save_links
+from src.scrape import get_recipe
+from db.json_db import *
+
+
+# from db.mongo_db import *
 
 
 def scrape_all_recipe_urls_from_menu():
     # from the cnofig.recipe menu dictionary
-    all_links = load_links()
+    all_links = load_json_to_set()
 
     # Scrape from recipe menus
     for menu_key, menu_path in recipe_menus.items():
@@ -14,41 +18,56 @@ def scrape_all_recipe_urls_from_menu():
         links = get_recipe_links_from_menu(menu_path)
         all_links.update(links)
 
-    save_links(all_links)
-    print(f"Scraping completed. Total recipes collected: {len(all_links)}")
-
-
-def scrape_all_recipe_urls_from_list():
-    # from the config.recipe_list
-    all_links = load_links()
-
     # Scrape from paginated recipe lists
     for list_path in recipe_lists:
         print(f"Scraping list: {list_path}")
         links = get_recipe_links_from_list(list_path)
         all_links.update(links)
 
-    save_links(all_links)
+    save_to_json(all_links)
     print(f"Scraping completed. Total recipes collected: {len(all_links)}")
 
 
 def scrape_all_recipe_urls_from_search():
     # from the website search site
-    all_links = load_links()
+    all_links = load_json_to_set()
 
     # Scrape from search list
     links = get_all_recipe_links()
     all_links.update(links)
 
-    save_links(all_links)
+    save_to_json(all_links)
     print(f"Scraping completed. Total recipes collected: {len(all_links)}")
 
 
-def scrape_all_recipes():
-    return None
+def main():
+    # Load URLs from JSON file
+    urls = load_json_to_set()
+    all_recipes = []
+
+    # Process each URL: scrape and save to MongoDB
+    for i, url in enumerate(urls):
+        # progress checking
+        if i % 10 == 0:
+            print(f"Scraping finshed for {i} item")
+
+        # scrape for the url
+        recipe_data = get_recipe(url)
+
+        # append if data exists
+        if recipe_data:
+            all_recipes.append(recipe_data)
+            print(recipe_data)
+        else:
+            print(f'Failed to save: {url}')
+
+    # Save all the recipes to json file
+    print(f'Total recipes collected: {len(all_recipes)}')
+    save_to_json(all_recipes, filename=f'db/{RECIPES_FILENAME}')
 
 
 if __name__ == '__main__':
     # scrape_all_recipe_urls_from_menu()
     # scrape_all_recipe_urls_from_list()
-    scrape_all_recipe_urls_from_search()
+    # scrape_all_recipe_urls_from_search()
+    main()
